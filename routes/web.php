79d0a3c24dc6bba65cda7dcc5a2bbc80017b57b9
littleredforest <devanssmile@gmail.com>
2016-10-19 20:56:19 -0500

    second commit

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::get('contact', function() {
 
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
+
+Route::get('articles', 'ArticlesController@index');
+Route::get('articles/{id}', 'ArticlesController@show');
