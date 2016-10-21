cade96dac086e12ce72d6b662c156215cd5f9c70
littleredforest <devanssmile@gmail.com>
2016-10-20 21:06:32 -0500

    third commit

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Request;
 
 use App\Http\Requests;
+use Carbon\Carbon;
 use App\Article;
 
 class ArticlesController extends Controller
 {
     public function index() {
-        $articles = Article::all();
+        $articles = Article::latest('published_at')->published()->get();
 
         return view('articles.index', compact('articles'));
     }
@@ -19,6 +20,16 @@ class ArticlesController extends Controller
         $article = Article::findOrFail($id);
 
         return view('articles.show', compact('article'));
+    }
+
+    public function create() {
+        return view('articles.create');
+    }
+
+    public function store() {
+        Article::create(Request::all());
+
+        return redirect('articles');
     }
     //
 }
